652134536371307fc90a68512122c5bdd37a0941
maxz2024 <maksimzdonov456@yandex.ru>
2024-01-31 11:06:09 +0300

    create expo app

---
+++ models/Header.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Header</Text>
+      <button className="button">Привет</button>
+    </View>
+  )
+}
+let but = document.getElementsByClassName("button")
+but.onclick = 
+console.log(but)
+export default Header
