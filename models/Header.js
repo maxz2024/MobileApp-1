926e62066550256091d1e1a69ad8f2a92d70842b
maxz2024 <maksimzdonov456@yandex.ru>
2024-03-05 11:19:21 +0300

    update Header.js

--- models/Header.js
+++ models/Header.js
@@ -5,11 +5,8 @@ const Header = () => {
   return (
     <View>
       <Text>Header</Text>
-      <button className="button">Привет</button>
     </View>
   )
 }
-let but = document.getElementsByClassName("button")
-but.onclick = 
-console.log(but)
+
 export default Header
